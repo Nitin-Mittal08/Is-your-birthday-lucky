78c65c10c62725abfde92d28d6c7de687a4849fb
Nitin Mittal <nitin2000mittal@gmail.com>
2022-12-02 00:42:56 +0530

    completed JS

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const checkButton = document.querySelector("check-number");
+const checkButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
 function isBirthdayLucky(){
-      const
+      const dob = dateOfBirth.value;
+      const sum = calculateSum(dob);
+      //console.log(sum);
+      compareValues(sum, luckyNumber.value)
+
 
 }
 
-checkButton.addEventListener("click", isBirthdayLucky);
+function compareValues(sum, luckyNumber){
+    if(sum %luckyNumber==0){
+        outputBox.innerText = "Your Birthday is Lucky🥳";
+    }
+    else{
+        outputBox.innerText = "Your Birthday isn't Lucky, anyways you make your own luck😎";
+    }
+}
+
+function calculateSum(dob){
+   dob =  dob.replaceAll("-","");
+    let sum = 0;
+
+    for(let i = 0;i<dob.length;i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+
+    return sum;
+}
+
+checkButton.addEventListener('click', isBirthdayLucky);
