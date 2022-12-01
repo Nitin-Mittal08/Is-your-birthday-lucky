a462a63039d0e1bbea503830a2c48ef160c08d63
Nitin Mittal <nitin2000mittal@gmail.com>
2022-12-01 23:59:44 +0530

    completed major css

---
+++ app.js
@@ -0,0 +1,10 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("check-number");
+
+function isBirthdayLucky(){
+      const
+
+}
+
+checkButton.addEventListener("click", isBirthdayLucky);
